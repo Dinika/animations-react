fb15e1f3c0018b58b1e1b28b1ea983a1d48cb518
Dinika <dinikasaxenas@gmail.com>
2019-04-04 21:37:41 +0530

    open close modal and backdrop
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./App.css";
-import Modal from "./components/Modal/Modal";
-import Backdrop from "./components/Backdrop/Backdrop";
-import List from "./components/List/List";
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
 
 class App extends Component {
+  state = {
+    isModalClosed: true
+  };
+
+  closeModal() {
+    this.setState({ isModalClosed: true });
+  }
+
+  openModal() {
+    this.setState({ isModalClosed: false });
+  }
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal
+          closed={this.state.isModalClosed}
+          closeModal={this.closeModal.bind(this)}
+        />
+        <Backdrop closed={this.state.isModalClosed} />
+        <button className="Button" onClick={this.openModal.bind(this)}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = props => {
+  const cssClasses = [
+    'Backdrop',
+    props.closed ? 'BackdropClose' : 'BackdropOpen'
+  ];
+  return <div className="Backdrop" className={cssClasses.join(' ')} />;
+};
 
 export default backdrop;
