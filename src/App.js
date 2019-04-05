d9c76babd56b9c591e64c159f16b14d52d77c333
Dinika <dinikasaxenas@gmail.com>
2019-04-06 00:37:23 +0530

    animate the modal in and out using react-transition-group
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Transition from 'react-transition-group/Transition';
 import './App.css';
 import Modal from './components/Modal/Modal';
 import Backdrop from './components/Backdrop/Backdrop';
@@ -21,10 +21,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal
-          closed={this.state.isModalClosed}
-          closeModal={this.closeModal.bind(this)}
-        />
+        <Transition
+          in={!this.state.isModalClosed}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => (
+            <Modal closed={state} closeModal={this.closeModal.bind(this)} />
+          )}
+        </Transition>
+
         <Backdrop closed={this.state.isModalClosed} />
         <button className="Button" onClick={this.openModal.bind(this)}>
           Open Modal
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './Modal.css';
 
 const modal = props => {
-  const cssClasses = ['Modal', props.closed ? 'ModalClose' : 'ModalOpen'];
+  const cssClasses = [
+    'Modal',
+    props.closed === 'exiting'
+      ? 'ModalClose'
+      : props.closed === 'entering'
+      ? 'ModalOpen'
+      : null
+  ];
   return (
     <div className="Modal" className={cssClasses.join(' ')}>
       <h1>A Modal</h1>
